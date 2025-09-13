2d30023e0e3cc1122002b4eea93eed1743db3f4a
ilclo <131272565+ilclo@users.noreply.github.com>
2025-09-13 09:23:22 +0200

    Update UiRenderer.kt

--- app/src/main/java/ai/runow/ui/renderer/UiRenderer.kt
+++ app/src/main/java/ai/runow/ui/renderer/UiRenderer.kt
@@ -75,7 +75,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.safeDrawing
