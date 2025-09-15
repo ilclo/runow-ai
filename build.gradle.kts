7bb292c06832b412d39f2e1f8308eb6c969056bc
ilclo <131272565+ilclo@users.noreply.github.com>
2025-09-15 15:34:26 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,4 @@
 plugins {
     id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    // (opzionali) se usi altri moduli/librerie, abilita anche:
-    // id("com.android.library") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
 }
-
