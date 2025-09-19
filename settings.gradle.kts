1defcf4ad2f084d22f27e4c212d9ae0c6e515df7
ilclo <131272565+ilclo@users.noreply.github.com>
2025-09-19 16:24:50 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,11 @@ pluginManagement {
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral(); maven { url "https://jitpack.io" } }
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") } // <-- Kotlin DSL corretta
+    }
 }
 rootProject.name = "runow-ai"
 include(":app")
